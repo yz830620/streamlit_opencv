eb5de7751222439b71262f9ebb42ff62b54dce9b
Even Pan <yz830620@gmail.com>
2022-02-02 17:49:34 +0800

    add basic service structure

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Custom imports 
+from multipage import MultiPage
+from pages import color_selector
+
+# Create an instance of the app 
+app = MultiPage()
+# Title of the main page
+st.title("OpenCV practice")
+
+# Add all your applications (pages) here
+app.add_page("opencv", color_selector.app)
+
+# The main app
+app.run()
---
+++ pages/color_selector.py
@@ -0,0 +1,11 @@
+"""
+# My opencv app
+DESC: app for opencv visualizing
+user inputs: R, G, B
+output: show picture with in threshold
+"""
+import streamlit as st
+
+def app():
+    st.subheader('Opencv color seletor')
+    st.caption('create by Even Pan, Date: 2022/2/2')
